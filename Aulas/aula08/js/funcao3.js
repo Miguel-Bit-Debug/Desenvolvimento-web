43403651d3e0bab72a7009e029b273cf654b49bd
Miguel-Bit-Debug <migueldossantos011@gmail.com>
2024-06-08 11:55:47 -0300

    Adicionando aula 08

---
+++ Aulas/aula08/js/funcao3.js
@@ -0,0 +1,12 @@
+var exibeMensagem = function(msg) {
+    console.log(msg)
+}
+
+// exibeMensagem('Hello world')
+
+// Arrow Function ECMAScript 6
+var exibeNome = (nome) => {
+    console.log(nome)
+}
+
+exibeNome('Fulano')
